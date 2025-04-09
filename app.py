d80d9d3b41d83c1c99d94230cf3dfdd340743cda
TROJANSCi <log.maksa@gmail.com>
2025-04-09 18:34:29 +0500

    1

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from schemas.theme_schema import Theme
 from utils.utils import ImageCreator
 
 ic = ImageCreator(tz='Asia/Tashkent', locales='ru_RU', theme=Theme(
-    font1='StickNoBills-ExtraBold.ttf',
-    font1_size=180,
-    font1_color='#ffffff',
-    add_date=False,
-    background='logite_soft.png',
-    background_behavior='static',
-    top_margin=-90
+    # font1='DigitalNumbers-Regular.ttf',
+    # font1_size=100,
+    # font1_color='#169e4d',
+    # add_date=False,
+    # # background='logite_soft.png',
+    # background_behavior='static',
+    # top_margin=-15
 )
                   )
 ic.img_generator(preview=True)
--- schemas/theme_schema.py
+++ schemas/theme_schema.py
@@ -4,13 +4,13 @@ from typing import Literal, Optional
 
 @dataclass
 class Theme:
-    font1: Optional[str] = field(default=None)
-    font1_size: Optional[float] = field(default=230)
-    font1_color: Optional[tuple | str] = field(default='#FF0000')
+    font1: Optional[str] = field(default='DigitalNumbers-Regular.ttf')
+    font1_size: Optional[float] = field(default=100)
+    font1_color: Optional[tuple | str] = field(default='#169e4d')
 
     font2: Optional[str] = field(default=None)
     font2_size: Optional[float] = field(default=14)
-    font2_color: Optional[tuple | str] = field(default='#fcba03')
+    font2_color: Optional[tuple | str] = field(default='#ffffff')
 
     add_date: Optional[bool] = field(default=False)
     date_format: Optional[str] = field(default='%a, %d %B')
